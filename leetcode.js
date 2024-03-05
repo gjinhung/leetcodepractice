c0b9d5d6e953ede7263caa62071e6fd855b102a0
gjinhung <gjinhung2@gmail.com>
2024-03-04 22:25:12 -0500

    3.4.24

--- leetcode.js
+++ leetcode.js
@@ -144,4 +144,18 @@ let isValid = function (s) {
     return (!stack.length > 0)
 };
 
-console.log(isValid("{[{}]}"))
+// console.log(isValid("{[{}]}"))
+
+
+let removeDuplicates = function (nums) {
+    let index = 1
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] != nums[i + 1]) {
+            nums[index] = nums[i + 1]
+            index++
+        }
+    }
+    return index
+};
+
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
