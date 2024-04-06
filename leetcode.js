f7ac4b92a2f9ad4cc3858dc91b613096411a7a5e
gjinhung <gjinhung2@gmail.com>
2024-04-05 22:20:02 -0400

    addtwo

--- leetcode.js
+++ leetcode.js
@@ -328,7 +328,6 @@ var divide = function (dividend, divisor) {
     } else if (dividend < 0 && divisor < 0) {
         negative = false
     }
-
     i = Math.abs(i)
     divisor = Math.abs(divisor)
     let count = 0
@@ -344,4 +343,37 @@ var divide = function (dividend, divisor) {
     else { return count }
 };
 
-console.log(divide(-1, -1))
+// console.log(divide(-1, -1))
+
+
+var addTwoNumbers = function (l1, l2) {
+    var List = new ListNode(0);
+    var head = List;
+    var sum = 0;
+    var carry = 0;
+
+    while (l1 !== null || l2 !== null || sum > 0) {
+
+        if (l1 !== null) {
+            sum = sum + l1.val;
+            l1 = l1.next;
+        }
+        if (l2 !== null) {
+            sum = sum + l2.val;
+            l2 = l2.next;
+        }
+        if (sum >= 10) {
+            carry = 1;
+            sum = sum - 10;
+        }
+
+        head.next = new ListNode(sum);
+        head = head.next;
+
+        sum = carry;
+        carry = 0;
+
+    }
+
+    return List.next;
+};
