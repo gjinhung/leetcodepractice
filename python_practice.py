1906a4fd0b3a628f16246697976fb1db9dca220f
gjinhung <gjinhung2@gmail.com>
2024-04-24 20:45:09 -0400

    python

---
+++ python_practice.py
@@ -0,0 +1,10 @@
+def is_same_num(num1, num2):
+  if num1 == num2:
+    return True
+  return False
+
+
+
+print(is_same_num(4, 8))   #>  False
+print(is_same_num(2, 2))   #>  True
+print(is_same_num(2, "2"))
