166f2a63e469faccc9ff6d4de8224017903ba3d5
gjinhung <gjinhung2@gmail.com>
2024-04-27 08:19:36 -0400

    2

--- python_practice.py
+++ python_practice.py
@@ -1,10 +1,28 @@
 def is_same_num(num1, num2):
-  if num1 == num2:
-    return True
-  return False
+    if num1 == num2:
+        return True
+    return False
 
 
+# print(is_same_num(4, 8))   #>  False
+# print(is_same_num(2, 2))   #>  True
+# print(is_same_num(2, "2"))
 
-print(is_same_num(4, 8))   #>  False
-print(is_same_num(2, 2))   #>  True
-print(is_same_num(2, "2"))
+
+def not_equal(num1, num2):
+    return num1 != num2
+
+
+# print(not_equal(0, 2))  # >  True
+# print(not_equal(2, 2))  # >  False
+# print(not_equal(2, "2"))
+
+
+def And(a, b):
+    return a and b
+
+
+print(And(True, False))  # > False
+print(And(True, True))  # > True
+print(And(False, True))  # > False
+print(And(False, False))  # > False
