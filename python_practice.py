88f8d0d062cfbd30d8b42571eab42aee9e7c43c3
gjinhung <gjinhung2@gmail.com>
2024-04-27 22:13:32 -0400

    4

--- python_practice.py
+++ python_practice.py
@@ -22,7 +22,33 @@ def And(a, b):
     return a and b
 
 
-print(And(True, False))  # > False
-print(And(True, True))  # > True
-print(And(False, True))  # > False
-print(And(False, False))  # > False
+# print(And(True, False))  # > False
+# print(And(True, True))  # > True
+# print(And(False, True))  # > False
+# print(And(False, False))  # > False
+
+
+def find_digit_amount(a):
+    return len(str((a * a) ** 0.5)) - 2
+    # return len(a)
+
+
+# print(find_digit_amount(123))  # > 3
+# print(find_digit_amount(-56))  # > 2
+# print(find_digit_amount(7154))  # > 4
+# print(find_digit_amount(61217311514))  # > 11
+# print(find_digit_amount(0))
+
+
+def recursive_fib(n):
+    if n <= 1:
+        return n
+    else:
+        return recursive_fib(n - 1) + recursive_fib(n - 2)
+
+
+print(recursive_fib(1))  # > 1
+print(recursive_fib(2))  # > 1
+print(recursive_fib(4))  # > 3
+print(recursive_fib(6))  # > 8
+print(recursive_fib(12))  # > 144
