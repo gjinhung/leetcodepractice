2dd8b629a1a3715369275fdbc29e0103c5107408
gjinhung <gjinhung2@gmail.com>
2024-08-02 19:39:07 -0400

    first_non_consecutive

--- python_practice.py
+++ python_practice.py
@@ -866,3 +866,11 @@ def find_smallest_int(arr):
     return smallest
 
     # return min(arr)
+
+def first_non_consecutive(arr):
+    for i in range(1, len(arr)):
+        if arr[i] != arr[i-1] + 1:
+            return arr[i]
+    return None
+
+print(first_non_consecutive([1,2,3,4]))
