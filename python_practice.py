1acbaef23d34e53e6daede78671ba7d6107c5f8b
gjinhung <gjinhung2@gmail.com>
2024-11-09 14:01:02 -0500

    hero

--- python_practice.py
+++ python_practice.py
@@ -1728,4 +1728,9 @@ def powers_of_two(n):
     return [2**x for x in range(n + 1)]
 
 
-print(powers_of_two(2))
+# print(powers_of_two(2))
+
+
+def hero(bullets, dragons):
+
+    return True if bullets / 2 > dragons else False
