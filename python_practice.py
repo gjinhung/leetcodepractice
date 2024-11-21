3e1049482efd406996b42040928e99fdbade744c
gjinhung <gjinhung2@gmail.com>
2024-11-20 20:21:58 -0500

    list squred optimized

--- python_practice.py
+++ python_practice.py
@@ -1785,3 +1785,18 @@ def list_squared(m, n):
 
 
 print(list_squared(1, 250))
+
+
+def list_squared(m, n):
+    result = []
+    for num in range(m, n + 1):
+        divisors = set()
+        for i in range(1, int(math.sqrt(num) + 1)):
+            if num % i == 0:
+                divisors.add(i**2)
+                divisors.add(int(num / i) ** 2)
+        total = sum(divisors)
+        sr = math.sqrt(total)
+        if sr - math.floor(sr) == 0:
+            result.append([num, total])
+    return result
